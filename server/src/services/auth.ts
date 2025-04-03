8d630c52b6d7ae331bf20646e1ac0dca7223af52
lunledev <183235731+lunledev@users.noreply.github.com>
2025-04-03 01:47:26 -0700

    initial commit

---
+++ server/src/services/auth.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import {GraphQLError} from 'graphql'
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const authenticationToken = ({req}): any) =>{
+
+    const authHeader =;
+    const bodyToken = ;
+    const queryToken = ;
+
+    
+}
