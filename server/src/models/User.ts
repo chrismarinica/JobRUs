463e0e12477894eabe875f02ed043cffd4afa4ed
lunledev <183235731+lunledev@users.noreply.github.com>
2025-04-05 00:01:07 -0700

    added User.ts

---
+++ server/src/models/User.ts
@@ -0,0 +1,70 @@
+import { Schema, model, Document } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+import jobSchema from './Jobs.js';
+import type {JobsDoc} from './Jobs.js';
+
+export interface UserDoc extends Document {
+    username: string;
+    email: string;
+    password: string;
+    savedJobs: jobDoc[];
+    isCorrectPassword(pasword: string): Promise<boolean>;
+    JobCount: number;
+}
+
+const userSchema = new Schema<UserDoc>(
+    {
+         username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+         },
+         email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+
+         },
+         password: {
+            type: String,
+            required: true,
+            minlength: 8,
+         },
+
+         savedJobs: [jobSchema],
+
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+
+);
+
+userSchema.pre<UserDoc>('save', async function(next) {
+    if(this.isNew || this.isModified('password')){
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+
+    }
+
+    next();
+
+});
+
+userSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
+    
+};
+
+userSchema.virtual('JobCount').get(function() {
+    return this.savedJobs.length;
+});
+
+const User = model<UserDoc>('User', userSchema);
+
+export default User;
